773c0c29be3cbb92b6f0e154d94da3ae711c0a0f
Hen-PC <henmanzur@gmail.com>
2021-04-19 19:21:40 +0300

    LAB 3 - Strings and Console Output

--- LAB3.py
+++ LAB3.py
@@ -8,13 +8,16 @@ print all of them to the screen
 3.then cehck if your name is stored inside this full string
 "hen idan ben dudu shimon yael adam shahar kristina hai"
 '''
+
 #1)
 full_name = "hen manzur"
 mail = "henmanzur@gmail.com"
 age = 27
 print("1.\nFull Name: " + full_name + "\nAge: " + str(age) + "\nEmail: " + mail)
+
 #2)
 print("\n2.\nFull name: " + full_name[::-1] + "\nAge: " + str(age*3))
+
 #3)
 names = '''
 hen idan ben dudu shimon yael adam shahar kristina hai
