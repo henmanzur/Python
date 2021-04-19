c2b5912ed630798b9d03d7fe586ca836b2be9f30
Hen-PC <henmanzur@gmail.com>
2021-04-19 10:17:23 +0300

    Math

---
+++ Exercise2_Math.py
@@ -0,0 +1,46 @@
+#                                     #Math
+# #num=5
+# #print("Your number is: " + str(num))
+# #print(type(num))
+# #num=num+2
+# #print("Your number is: " + str(num))
+# #num2=num*10
+# #print("Your number is: " + str(num2))
+#
+# #################################
+# # * כפל | + ועוד | - פחות | ** חזקה | / חילוק | // מחלק במספרים שלמים ללא השארית | % חילוק והצגה של השארית |
+# #################################
+#                                 #דוגמאות
+
+# number = 0
+
+# #חיבור
+# number = number + 1
+# number += 1
+# print(number)
+
+# #חיסור
+# number = 4 -1
+# number -= 1
+# print(number)
+
+# #כפל
+# number *= 2
+# number = number * 1
+# print(number)
+
+# #חילוק
+# number /= 2
+# number = number / 1
+# print(number)
+
+# #שימוש בסוגריים
+# number = (1+3) * 3+4/2
+# print(number)
+
+# #חזקות
+# number **= 2 ** 2
+
+# #חלוקה עם שארית
+# number = number % 3
+# print(number)
