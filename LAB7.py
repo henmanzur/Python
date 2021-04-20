925d8c34d73691111d156bd2fb844165b7d3a3c0
Hen-PC <henmanzur@gmail.com>
2021-04-20 20:05:06 +0300

    LAB 7 - Cube Project

--- LAB7.py
+++ LAB7.py
@@ -15,15 +15,18 @@ print("\n-------------------------------\n🎲 Welcome to my Cube game! 🎲\n  
 money = int(input("Enter How much money do you have: "))
 turns =(money//3)
 
-sleep(3)
+sleep(2)
 if(money >= 3):
     print("-------------------------------\nYou have: " + str(turns) + " turns\nYour change: " + str(money % 3) + " ILS\n")
-    print("Lets Play!\n-------------------------------")
+    print("Lets Play!\n\n-------------------------------")
+    sleep(3)
 else:
     print("\n-------------------------------\nYou don't have enough money to play the cubes game.\n-------------------------------")
 
+price = 0
 for i in range(turns):
     print("Round Number " + str(i+1) + " :")
+    print("The dealer threw the cubes...\n")
     sleep(3)
     cube1 = randint(1, 6)
     cube2 = randint(1, 6)
@@ -34,19 +37,21 @@ for i in range(turns):
     a40 = 40
     a20 = 20
     if (cube1 == 6 and cube2 == 6):
-        print("-------------------------------\nYou won " + str(a1000) + " ILS !")
-        print("You have now: " + str(money % 3 + int(a1000)) + " ILS, includes your change.\n-------------------------------\n")
+        print("\nYou won " + str(a1000) + " ILS !")
+        print("You have now: " + str(money % 3 + int(a1000)) + " ILS, includes your change.\n-------------------------------")
     elif (cube1 == cube2):
-        print("-------------------------------\nYou won " + str(a100) + " ILS !")
-        print("You have now: " + str(money % 3 + int(a100)) + " ILS, includes your change.\n-------------------------------\n")
+        print("\nYou won " + str(a100) + " ILS !")
+        print("You have now: " + str(money % 3 + int(a100)) + " ILS, includes your change.\n-------------------------------")
     elif (cube1 == 1 and cube2 == 2):
-        print("-------------------------------\nYou won " + str(a60) + " ILS !")
-        print("You have now: " + str(money % 3 + int(a60)) + " ILS, includes your change.\n-------------------------------\n")
+        print("\nYou won " + str(a60) + " ILS !")
+        print("You have now: " + str(money % 3 + int(a60)) + " ILS, includes your change.\n-------------------------------")
     elif (cube1 != cube2 and cube1 == 1):
-        print("-------------------------------\nYou won " + str(a20) + " ILS !")
-        print("You have now: " + str(money % 3 + int(a20)) + " ILS, includes your change.\n-------------------------------\n")
+        print("\nYou won " + str(a20) + " ILS !")
+        print("You have now: " + str(money % 3 + int(a20)) + " ILS, includes your change.\n-------------------------------")
     elif (cube2 != cube1 and cube2 == 2):
-        print("-------------------------------\nYou won " + str(a40) + " ILS !")
-        print("You have now: " + str(money % 3 + int(a40)) + " ILS, includes your change.\n-------------------------------\n")
+        print("\nYou won " + str(a40) + " ILS !")
+        print("You have now: " + str(money % 3 + int(a40)) + " ILS, includes your change.\n-------------------------------")
     else:
-        print("-------------------------------\nYou lose !\n-------------------------------\n")
+        print("\nYou lose !\n-------------------------------")
+
+print("\nGood Bye!\n")
