db76af6b05205fc07fc211f699553be805d656bc
Hen-PC <henmanzur@gmail.com>
2021-04-24 08:46:09 +0300

    End Exercises

--- Project_H.W/End Exercises/Exercise_1.py
+++ Project_H.W/End Exercises/Exercise_1.py
@@ -18,3 +18,11 @@ Twinkle, twinkle, little star,
 
 print("\nTwinkle, twinkle, little star,\n       How I wonder what you are!\n              Up above the world so high,\n" \
       "              Like a diamond in the sky.\nTwinkle, twinkle, little star,\n       How I wonder what you are")
+
+
+print('''\nTwinkle, twinkle, little star,
+	How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	How I wonder what you are''')
---
+++ Project_H.W/End Exercises/Exercise_8.py
@@ -0,0 +1,13 @@
+'''
+####################
+#  End exercises   #
+####################
+8. Write a Python program to display the first and last colors from the following
+list.
+color_list = ["Red","Green","White" ,"Black"]
+
+'''
+
+color_list = ["Red","Green","White" ,"Black"]
+print("\nThis is my color list:\n" + str(color_list) + "\n")
+print("The first color: " + color_list[0] + "\nThe last color: " + color_list[-1])
