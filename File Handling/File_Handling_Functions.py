7f4324fd71141bb18a7c0f6632ff3488c7179645
Hen-PC <henmanzur@gmail.com>
2021-04-26 00:24:46 +0300

    File Handling - Create,Read,Write,Append,Delete Functions and Menu

--- File Handling/File_Handling_Functions.py
+++ File Handling/File_Handling_Functions.py
@@ -1,6 +1,15 @@
 #############################
 # File Handling - Functions #
 #############################
+import os
+
+def delete_file(file):
+    filename = "C:/Users/Hen-PC/Desktop/Networking Course/Python/pythonProject/Python_Net4U_/File Handling/Files/" + file + ".txt"
+    if os.path.exists(filename):
+        os.remove(filename)
+        print("\nFile Deleted")
+    else:
+        print("\nThe file does not exist")
 
 def create_file(file):
     f = open("C:/Users/Hen-PC/Desktop/Networking Course/Python/pythonProject/Python_Net4U_/File Handling/Files/" + file + ".txt" , "x")
@@ -26,7 +35,7 @@ def append_file(file):
 
 def menu():
     while(True):
-        choice=input("Enter your choice:\n1.Create a file\n2.Read a file\n3.Write a file\n4.Append to a file\n")
+        choice=input("\nMenu:\n-----\n1.Create a file\n2.Read a file\n3.Write a file\n4.Append to a file\n5.Delete a file\nEnter your choise: ")
         if(choice=="1"):
             create_file(input("Enter file name: "))
         elif(choice=="2"):
@@ -35,8 +44,10 @@ def menu():
             write_file(input("Enter file name: "))
         elif(choice=="4"):
             append_file(input("Enter file name: "))
+        elif (choice == "5"):
+            delete_file(input("Enter file name: "))
         else:
-            print("Enter 1-4 Only !")
+            print("Enter 1-5 Only !")
 
 
 ########Main Code##########
