03920846c6e716910af628ea79735a00b2e240bd
Hen-PC <henmanzur@gmail.com>
2021-04-26 09:47:03 +0300

    LAB 10 - Main Code

---
+++ Labs/Lab10/LAB10.py
@@ -0,0 +1,27 @@
+'''
+Create a menu:   #  ✅
+a. IP System?    #  ✅
+b. DNS System?   #  ✅
+
+a ### כל כתובות האייפי יהיו בקובץ טקסט נפרד
+====
+    1. search for IP address from a list
+    2. add IP address to a list
+    3. Delete IP address to a list
+    4. Print all the IPs to the screen
+
+b
+====
+    1. search for URL from a dictionary      # ✅
+    2. add URL + IP address to a dictionary  # ✅
+    3. Delete URL from a dictionary          # ✅
+    4. Update the IP address of specific URL # ✅
+    5. Print all the URL:IP to the screen    # ✅
+
+            כל הפונקציות יהיו בקובץ פייטון LAB10_Functions.py ונמשוך אותם משם לקובץ LAB10.py
+'''
+
+####################################################################### -  MAIN CODE - #######################################################################
+from Labs.Lab10.LAB10_Functions import *
+
+Main_Menu()
